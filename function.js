bf5937b949d67c6c4730d957e71ca8f317ce58af
FxaverD <siskus1602@gmail.com>
2026-02-02 15:15:36 +0700

    notes

--- function.js
+++ function.js
@@ -1,1 +1,23 @@
 console.log ('hello world')
+// to register the git, git init
+
+// if there's any .js to add, add blablabla.js
+// 
+// if there's any changes to the coding
+// to add the git, git add .
+// to commit, git commit -m "add notes what changed"
+//PS C:\Users\fxave\OneDrive\Documents\purwadhika full stack\intro git> git add function.js
+
+// for example:
+//PS C:\Users\fxave\OneDrive\Documents\purwadhika full stack\intro git> git add .
+//PS C:\Users\fxave\OneDrive\Documents\purwadhika full stack\intro git> git commit -m "add new console in line 3"
+
+// check all commit and notes, git log
+
+// git revert menghapus pergantian sesuai dengan code pergantian yang kita cantumkan, tetapi perlu kita commit lagi untuk di save
+// for ex: git revert 7dfb9ee125001435e44761eda90576fbca227a3a  itu adalah commmit code yang ada di github history
+
+// git reset code yang dipakai adalah code yang dituju, sedangkan revert adalah code yang mau dihapus
+// for ex: git reset --hard <code> ini akan dihapus semua even the history of changes ny, jadi lebih bahaya
+// for ex: git reset --soft <code> ini akan menghapus semua commit, tetapi tidak menghapus yang di vs code, kalau tidak jadi, tinggal di commit lgi saja
+
